9521b368fcadb5a76068aa64665e546c9da30f6e
Ariel Tubul <avarti765@gmail.com>
2023-07-25 22:06:54 +0300

    created pdf splitter

---
+++ PdfSearch/PdfPagesIterator.cpp
@@ -0,0 +1,48 @@
+#include "PdfPagesIterator.hpp"
+
+#include <memory>
+#include <string>
+#include <opencv2/opencv.hpp>
+#include <poppler/cpp/poppler-page.h>
+#include <poppler/cpp/poppler-image.h>
+#include <poppler/cpp/poppler-document.h>
+#include <poppler/cpp/poppler-page-renderer.h>
+
+PdfPagesIterator::PdfPagesIterator(
+    const std::string& pdf_path,
+    const std::shared_ptr<poppler::page_renderer>& renderer):
+	_current_page_index(),
+    _pdf_document(poppler::document::load_from_file(pdf_path)),
+    _renderer(renderer)
+{
+}
+
+void PdfPagesIterator::iterate_to_next_page()
+{
+    ++_current_page_index;
+}
+
+bool PdfPagesIterator::has_more_pages() const
+{
+    return static_cast<size_t>(_pdf_document->pages()) != _current_page_index;
+}
+
+cv::Mat PdfPagesIterator::get_current_page() const
+{
+	std::unique_ptr<poppler::page> current_page{ _pdf_document->create_page(_current_page_index) };
+
+	static constexpr double PAGE_DPI = 200;
+	poppler::image current_page_image = _renderer->render_page(current_page.get(), PAGE_DPI, PAGE_DPI);
+
+	cv::Mat notOwningImageMat{
+		current_page_image.height(),
+		current_page_image.width(),
+		CV_8UC3,
+		current_page_image.data(),
+		static_cast<size_t>(current_page_image.bytes_per_row()) };
+
+	cv::Mat tempOwningMat;
+	notOwningImageMat.copyTo(tempOwningMat);
+
+	return tempOwningMat;
+}
---
+++ PdfSearch/PdfPagesIterator.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include <opencv2/opencv.hpp>
+#include <poppler/cpp/poppler-document.h>
+#include <poppler/cpp/poppler-page-renderer.h>
+
+class PdfPagesIterator
+{
+public:
+	explicit PdfPagesIterator(
+		const std::string& pdf_path, 
+		const std::shared_ptr<poppler::page_renderer>& renderer);
+
+	void iterate_to_next_page();
+	bool has_more_pages() const;
+	cv::Mat get_current_page() const;
+
+private:
+	size_t _current_page_index;
+	const std::unique_ptr<poppler::document> _pdf_document;
+	const std::shared_ptr<poppler::page_renderer> _renderer;
+};
