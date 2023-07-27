6e9af22db0d78803c28c8dd570ed8b14e55d7ba8
Ariel Tubul <avarti765@gmail.com>
2023-07-27 19:55:10 +0300

    bugfixes

--- PdfSearch/ThreadSafeQueue.hpp
+++ PdfSearch/ThreadSafeQueue.hpp
@@ -8,29 +8,39 @@ template <typename T>
 class ThreadSafeQueue final
 {
 public:
-	void enqueue(T item)
-	{
-		std::lock_guard<std::mutex> lock(_mutex);
+	ThreadSafeQueue() :
+		_should_quit(false)
+	{}
 
+public:
+	void enqueue(const T& item)
+	{
+		std::unique_lock<std::mutex> lock(_mutex);
 		_queue.push(item);
+
+		lock.unlock();
 		_conditional_variable.notify_one();
 	}
 
 	T dequeue()
 	{
 		std::unique_lock<std::mutex> lock(_mutex);
+		_conditional_variable.wait(lock, [this] { return !_queue.empty() || _should_quit; });
 
-		_conditional_variable.wait(lock,
-			[&]() { return _should_quit || !_queue.empty();});
+		if (_queue.empty())
+		{
+			throw std::runtime_error("Quit signaled during dequeue");
+		}
 
-		T item = _queue.front();
+		T front = _queue.front();
 		_queue.pop();
 
-		return item;
+		return front;
 	}
 
 	bool empty() const
 	{
+		std::lock_guard<std::mutex> lock_guard{ _mutex };
 		return _queue.empty();
 	}
 
@@ -46,7 +56,7 @@ public:
 	}
 
 private:
-	std::mutex _mutex;
+	mutable std::mutex _mutex;
 	std::queue<T> _queue;
 	std::atomic<bool> _should_quit;
 	std::condition_variable _conditional_variable;
