a93396812e36fc3693328b77676e792ee5579eff
Ariel Tubul <avarti765@gmail.com>
2023-07-27 23:51:49 +0300

    impl threadsafequeue otra vez

--- PdfSearch/ThreadSafeQueue.hpp
+++ PdfSearch/ThreadSafeQueue.hpp
@@ -1,63 +1,79 @@
 #pragma once
 
-#include <queue>
 #include <mutex>
+#include <atomic>
+#include <queue>
+#include <stdexcept>
 #include <condition_variable>
 
-template <typename T>
-class ThreadSafeQueue final
+template<typename T>
+class ThreadSafeQueue
 {
 public:
-	ThreadSafeQueue() :
-		_should_quit(false)
-	{}
+    explicit ThreadSafeQueue(size_t max_size)
+        : _max_size(max_size), _should_quit(false) {}
 
 public:
-	void enqueue(const T& item)
-	{
-		std::unique_lock<std::mutex> lock(_mutex);
-		_queue.push(item);
+    void enqueue(const T& item)
+    {
+        std::unique_lock<std::mutex> lock(_mutex);
+        _condition_queue_insert.wait(lock, [this] { return _queue.size() < _max_size || _should_quit; });
 
-		lock.unlock();
-		_conditional_variable.notify_one();
-	}
+        if (_should_quit)
+        {
+            throw std::runtime_error("Quit signaled during insert");
+        }
 
-	T dequeue()
-	{
-		std::unique_lock<std::mutex> lock(_mutex);
-		_conditional_variable.wait(lock, [this] { return !_queue.empty() || _should_quit; });
+        _queue.push(item);
+        _condition_queue_removal.notify_one();
+    }
 
-		if (_queue.empty())
-		{
-			throw std::runtime_error("Quit signaled during dequeue");
-		}
+    T dequeue()
+    {
+        std::unique_lock<std::mutex> lock(_mutex);
+        _condition_queue_removal.wait(lock, [this] { return !_queue.empty() || _should_quit; });
 
-		T front = _queue.front();
-		_queue.pop();
+        if (_queue.empty())
+        {
+            throw std::runtime_error("Quit signaled during removal");
+        }
 
-		return front;
-	}
+        T item = _queue.front();
+        _queue.pop();
 
-	bool empty() const
-	{
-		std::lock_guard<std::mutex> lock_guard{ _mutex };
-		return _queue.empty();
-	}
+        _condition_queue_insert.notify_one();
+        return item;
+    }
 
-	void signal_quit()
-	{
-		_should_quit = true;
-		_conditional_variable.notify_one();
-	}
+    void signal_quit() 
+    {
+        _should_quit = true;
+        _condition_queue_removal.notify_all();
+        _condition_queue_insert.notify_all();
+    }
 
-	bool should_quit() const
-	{
-		return _should_quit;
-	}
+    bool empty() const
+    {
+        std::lock_guard<std::mutex> lock(_mutex);
+        return _queue.empty();
+    }
+
+    size_t size() const
+    {
+        std::lock_guard<std::mutex> lock(_mutex);
+        return _queue.size();
+    }
+
+    bool should_quit() const
+    {
+        return _should_quit;
+    }
 
 private:
-	mutable std::mutex _mutex;
-	std::queue<T> _queue;
-	std::atomic<bool> _should_quit;
-	std::condition_variable _conditional_variable;
+    std::queue<T> _queue;
+    mutable std::mutex _mutex;
+    std::condition_variable _condition_queue_removal;
+    std::condition_variable _condition_queue_insert;
+    std::atomic<bool> _should_quit;
+    const size_t _max_size;
 };
